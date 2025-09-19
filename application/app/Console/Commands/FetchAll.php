52228f119add44b307a9ae4032a218074c633ea5
N.Brodatskiy <nikolaybrodatskiy@gmail.com>
2025-09-19 17:43:44 +0300

    Добавляет команду консоли для извлечения всех данных с сервера.

---
+++ application/app/Console/Commands/FetchAll.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class FetchAll extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'fetch:all';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Fetch all data from remote server';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle(): int
+    {
+        $this->call('fetch:incomes');
+        $this->call('fetch:orders');
+        $this->call('fetch:sales');
+        $this->call('fetch:stocks');
+
+        return 0;
+    }
+}
