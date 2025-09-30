47b6275ba7818d338fe11fe106aa989775d4b055
N.Brodatskiy <nikolaybrodatskiy@gmail.com>
2025-09-30 15:50:08 +0300

    Добавляет команду консоли для создания новой компании.

---
+++ application/app/Console/Commands/CreateCompany.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Company;
+use Illuminate\Console\Command;
+
+class CreateCompany extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'company:create {name : The name of the company}';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create new company';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle(): int
+    {
+        Company::create([
+            'name' => $this->argument('name'),
+        ]);
+
+        $this->info("Компания " . $this->argument('name') . " создана");
+
+        return 0;
+    }
+}
