56c2c9be8f3147ea4829daeeae38c093842b09ec
N.Brodatskiy <nikolaybrodatskiy@gmail.com>
2025-09-30 15:49:42 +0300

    Добавляет PHPDoc для метода создания в классе Company.

--- application/app/Models/Company.php
+++ application/app/Models/Company.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * @method static create(array $array)
+ */
 class Company extends Model
 {
     use SoftDeletes;
