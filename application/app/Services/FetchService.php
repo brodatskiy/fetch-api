df9987f5213fb9de0f0114b40a6a1bf70adf60c7
N.Brodatskiy <nikolaybrodatskiy@gmail.com>
2025-09-30 12:07:49 +0300

    Добавляет обработку ошибок и логирование в FetchService.

--- application/app/Services/FetchService.php
+++ application/app/Services/FetchService.php
@@ -7,14 +7,13 @@ use App\Jobs\SaveData;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
 use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Log;
 
 class FetchService
 {
     private int $page = 1;
     const LIMIT = 500;
     private int $backoffDelay = 60;
-    private int $backoffAttempt = 0;
-    private int $backoffMaxAttempt = 5;
     private Client $client;
 
     public function __construct()
@@ -25,41 +24,58 @@ class FetchService
         ]);
     }
 
-    /**
-     * @throws GuzzleException
-     */
     public function fetch(string $modelName, string $uri, string $dateFrom, string $dateTo): void
     {
         do {
-            $response = $this->client->request('GET', $uri, [
-                'query' => [
-                    'dateFrom' => $dateFrom,
-                    'dateTo' => $dateTo,
-                    'key' => Config::get('services.fetch_api.key'),
-                    'page' => $this->page,
-                    'limit' => self::LIMIT,
-                ],
-            ]);
+            $response = null;
+            try {
+                $response = $this->client->request('GET', $uri, [
+                    'query' => [
+                        'dateFrom' => $dateFrom,
+                        'dateTo' => $dateTo,
+                        'key' => Config::get('services.fetch_api.key'),
+                        'page' => $this->page,
+                        'limit' => self::LIMIT,
+                    ],
+                ]);
 
-            $hasMorePages = $data['meta']['last_page'] > $this->page ?? false;
-            [$remainingRequests] = $response->getHeader('X-RateLimit-Remaining');
-
-            if ($remainingRequests == 0) {
-                $this->waitForBackoff();
-                continue;
+            } catch (GuzzleException $e) {
+                $this->logRequest($modelName, $uri, $dateFrom, $dateTo, $e);
+                break;
             }
 
             $data = json_decode($response->getBody()->getContents(), true);
+            $hasMorePages = $data['meta']['last_page'] > $this->page ?? false;
+
+            [$remainingRequests] = $response->getHeader('X-RateLimit-Remaining');
+
+            if ($remainingRequests == 0) {
+                sleep($this->backoffDelay);
+                continue;
+            }
+
             SaveData::dispatch($modelName, $data);
             $this->page++;
-            $this->backoffAttempt = 0;
         } while ($hasMorePages);
     }
 
-    private function waitForBackoff(): void
+    protected function logRequest(string $modelName, string $uri, string $dateFrom, string $dateTo, GuzzleException $e): void
     {
-        $delay = $this->backoffDelay * pow(2, $this->backoffAttempt);
-        $this->backoffAttempt++;
-        sleep($delay);
+        $statusCode = $e->getCode();
+        $statusMessage = $e->getMessage();
+        $message = sprintf(
+            "\nModel name: %s, \nURI: %s, \nDateFrom: %s, \nDateTo: %s, \nPage: %d, \nStatus code: %d, \nStatus message: \n%s",
+            $modelName,
+            $uri,
+            $dateFrom,
+            $dateTo,
+            $this->page,
+            $statusCode,
+            $statusMessage
+        );
+
+        if ($statusCode >= 400) {
+            Log::error($message);
+        }
     }
 }
