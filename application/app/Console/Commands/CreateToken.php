c7159d07ee744e53baa82c369f1307b7a4267191
N.Brodatskiy <nikolaybrodatskiy@gmail.com>
2025-09-30 20:02:30 +0300

    Добавлен класс команды для создания нового токена.

---
+++ application/app/Console/Commands/CreateToken.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Token;
+use Illuminate\Console\Command;
+
+class CreateToken extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'token:create {token_type_id} {token_value} {account_id} {api_service_id}';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create new token';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle(): int
+    {
+        $token = Token::create([
+            'token_type_id' => $this->argument('token_type_id'),
+            'token_value' => $this->argument('token_value'),
+            'account_id' => $this->argument('account_id'),
+            'api_service_id' => $this->argument('api_service_id'),
+        ]);
+
+        return 0;
+    }
+}
