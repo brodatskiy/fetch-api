b8b2867955dcc4fc0426d95dc5a8f6c7735f2ea3
N.Brodatskiy <nikolaybrodatskiy@gmail.com>
2025-09-30 19:08:13 +0300

    Добавил поддержку новых полей и методов в модель ApiService.

--- application/app/Models/ApiService.php
+++ application/app/Models/ApiService.php
@@ -2,21 +2,45 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * @method static create(array $array)
+ */
 class ApiService extends Model
 {
     use SoftDeletes;
 
     protected $fillable = [
         'name',
+        'host',
+        'supported_token_types',
+        'endpoints',
+    ];
+
+    protected $casts = [
+        'endpoints' => 'array',
+        'supported_token_types' => 'array',
     ];
 
     public function tokens(): HasMany
     {
         return $this->hasMany(Token::class);
+    }
+
+    public function addEndpoints(array $newEndpoints): bool
+    {
+        $endpoints = $this->endpoints ?? [];
+
+        foreach ($newEndpoints as $endpoint) {
+            if (!in_array($endpoint, $endpoints)) {
+                $endpoints[] = $endpoint;
+            }
+        }
+
+        $this->endpoints = $endpoints;
+        return $this->save();
     }
 }
