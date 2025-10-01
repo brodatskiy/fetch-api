b117eae3f608eba0b673a26215b4388ce54c7471
N.Brodatskiy <nikolaybrodatskiy@gmail.com>
2025-10-01 16:58:09 +0300

    Добавлен метод tokenTypes для отношения BelongsToMany.

--- application/app/Models/ApiService.php
+++ application/app/Models/ApiService.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -32,5 +33,10 @@ class ApiService extends Model
     public function endpoints(): HasMany
     {
         return $this->hasMany(Endpoint::class);
+    }
+
+    public function tokenTypes(): BelongsToMany
+    {
+        return $this->belongsToMany(TokenType::class);
     }
 }
