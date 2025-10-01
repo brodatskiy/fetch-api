725583583b9db66e8acc5afee642ff029eb5cb93
N.Brodatskiy <nikolaybrodatskiy@gmail.com>
2025-10-01 12:03:27 +0300

    Создал миграцию для таблицы endpoints.

---
+++ application/database/migrations/2025_10_01_085522_create_endpoints_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Models\ApiService;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEndpointsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('endpoints', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('uri');
+            $table->string('model');
+            $table->foreignIdFor(ApiService::class)->index()
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();;;
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('endpoints');
+    }
+}
