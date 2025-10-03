f0314717f3b38e8c146ef39b08de3cfc8caec3bf
N.Brodatskiy <nikolaybrodatskiy@gmail.com>
2025-10-04 01:29:14 +0300

    Добавил методы для разрешения отношений в модели Account. Добавлен метод для получения отношения по имени модели

--- application/app/Models/Account.php
+++ application/app/Models/Account.php
@@ -5,7 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Str;
 
 /**
  * @method static create(array $array)
@@ -18,6 +20,17 @@ class Account extends Model
         'name',
     ];
 
+    public function resolveRelation($modelName)
+    {
+        $methodName = strtolower(Str::plural($modelName));
+
+        if (method_exists($this, $methodName)) {
+            return $this->$methodName();
+        }
+
+        return null;
+    }
+
     public function company(): BelongsTo
     {
         return $this->belongsTo(Company::class);
@@ -26,5 +39,25 @@ class Account extends Model
     public function tokens(): HasMany
     {
         return $this->hasMany(Token::class);
+    }
+
+    public function incomes(): MorphToMany
+    {
+        return $this->morphedByMany(Income::class, 'accountable');
+    }
+
+    public function sales(): MorphToMany
+    {
+        return $this->morphedByMany(Sale::class, 'accountable');
+    }
+
+    public function orders(): MorphToMany
+    {
+        return $this->morphedByMany(Order::class, 'accountable');
+    }
+
+    public function stocks(): MorphToMany
+    {
+        return $this->morphedByMany(Stock::class, 'accountable');
     }
 }
